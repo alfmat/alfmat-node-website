86ef1c89766c27c90c02e5b920b4a5374c57d2c9
Alfred Mathew <alfredmathew718@gmail.com>
2021-06-28 23:55:15 -0400

    major overhaul of personal website

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.get('/resume', (req, res) => {
+app.get('/Alfred_Mathew_Resume', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/Alfred_Mathew_Resume.pdf'))
 });
 
